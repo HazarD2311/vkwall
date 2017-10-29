3821a154d6c9b77f658fea642e440d57034158d9
Alexey <hazard231196@gmail.com>
2017-10-29 17:49:45 +0300

    добавил навигацию вперед/назад с плавной анимацией

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
 function getAccessToken(){
-	return 'be16c1e9b17efe75ca0d276933420b928533e1bf130af69402ec6dd0d92d0ac2a3b63d57fe5742bb9ff41';
+	return 'e67c4659d4e7df2d12ff729436d3e8dca992d9972a47eea834c96ebf1f6d83b52b1ca630171a33df5b438';
 }
 
+//id человека, чью стену хотим посмотреть.
 function getOwnerId() {
 	//return 15910946;
 	return 53083705;
 	//return 1;
-	//return 37606557;
 }
 
+//кол-во записей получаемых после запроса
+function getCountOfNotes() {
+	return 10;
+}
+
+//синглтон для хранения номера показанных записей (номера первой показанной записи)
+var counterNote = (function () {
+
+	var instance,
+		counter = 0;
+
+	var getCounter = function () {
+		return counter;
+	}
+
+	var increaseCounter = function () {
+		counter += 10;
+	}
+
+	var decreaseCounter = function () {
+		counter -= 10;
+	}
+
+	var createInstance = function () {
+		return {
+			getCounter: getCounter,
+			increaseCounter: increaseCounter,
+			decreaseCounter: decreaseCounter
+		}
+	}
+
+	return {
+		getInstance: function () {
+			if (!instance)
+				return instance = createInstance();
+			else
+				return instance;
+			//return instance || (instance = createInstance);
+		}
+	}
+
+}());
+
 $('#btnWall').on('click', loadWall);
+$('#btnBack').on('click', back);
+$('#btnForward').on('click', forward);
 
 function getUrl(method, params){
 	if (!method) throw new Error('Укажите метод!');
@@ -28,21 +73,21 @@ function sendRequest(method, params, getData) {
 }
 
 function loadWall(){
-	sendRequest('wall.get', {count: 5, owner_id: getOwnerId}, function getData(data){
+	console.log(counterNote.getInstance().getCounter());
+	sendRequest('wall.get', {count: getCountOfNotes, owner_id: getOwnerId, offset: counterNote.getInstance().getCounter()}, function getData(data){
 		showWall(data.response.items);
-		console.log(data.response);
-		//console.log(data.response.items.length);
+		console.log(data.response.items);
 	});
 }
 
-function showWall(wall) {
+function showWall(wall, i) {
 	var html = '';
-	for (var i = 0; i < wall.length - 1; i++) {
+	for (i = 0; i < wall.length - 1; i++) {
 		var note = wall[i];
 		
 		html += 
 		'<li>'
-			+ '<div>'
+			+ '<div id="noteDiv">'
 		 		+ '<h4> <a target="_blank" href="https://vk.com/id' + note.owner_id + '?w=wall' + note.owner_id + '_' + note.id + '">' + 'Перейти на запись' + '</a> </h4>'
 		 		+ "<br/>"
 		 		+ note.text
@@ -57,3 +102,50 @@ function showWall(wall) {
 
 	$('ul').html(html);
 }
+
+function back() {
+	if (counterNote.getInstance().getCounter() < 10) return;
+
+	console.log(counterNote.getInstance().getCounter());
+	console.log('back: ' + counterNote.getInstance().getCounter);
+	counterNote.getInstance().decreaseCounter();
+
+	sendRequest('wall.get', {count: getCountOfNotes, owner_id: getOwnerId, offset: counterNote.getInstance().getCounter()}, function getData(data){
+		showWall(data.response.items);
+	});
+}
+
+function forward() {
+	console.log(counterNote.getInstance().getCounter());
+	console.log('forward: ' + counterNote.getInstance().getCounter);
+	counterNote.getInstance().increaseCounter();
+
+	sendRequest('wall.get', {count: getCountOfNotes, owner_id: getOwnerId, offset: counterNote.getInstance().getCounter()}, function getData(data){
+		showWall(data.response.items);
+	});
+}
+
+//плавный переход наверх при нажатии на кнопку Дальше
+$(function() {
+
+ $(window).scroll(function() {
+
+ if($(this).scrollTop() != 0) {
+
+ $('#navigation').fadeIn();
+ 
+ } else {
+
+ $('#navigation').fadeOut();
+
+ }
+
+ });
+ 
+ $('#navigation').click(function() {
+
+ $('body,html').animate({scrollTop:0},800);
+
+ });
+ 
+});
